9d5db178f88d6532ae3c95fa9fc55a5d5ca2bfde
Dmitry Zhukovsky <dmitry.zhukovsky@solgari.com>
2025-02-26 03:44:03 +0100

    refactor build process: remove esbuild.config.js and integrate build tasks into vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,43 @@
-import { defineConfig } from "vite";
 import fg from "fast-glob";
 import react from "@vitejs/plugin-react-swc";
+import { build } from "esbuild";
+import { copy } from "esbuild-plugin-copy";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [react()],
+  plugins: [react(), buildTasks()],
   build: {
     rollupOptions: {
       input: fg.sync("*.html"),
     },
   },
 });
+
+function buildTasks() {
+  return {
+    name: "build-tasks",
+    async closeBundle() {
+      await build({
+        entryPoints: ["tasks/**/*.ts"],
+        bundle: true,
+        platform: "node",
+        outdir: "dist",
+        outbase: ".",
+        format: "cjs",
+        minify: true,
+        logLevel: "info",
+        plugins: [
+          copy({
+            assets: [
+              {
+                from: "tasks/**/*.json",
+                to: "tasks",
+              },
+            ],
+          }),
+        ],
+      });
+    },
+  };
+}
